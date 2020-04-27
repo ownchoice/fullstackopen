1d64dcc676b8870b3ff2da8e35357002e1b827cc
ownchoice <ownchoice@users.noreply.github.com>
2020-04-27 00:50:32 -0300

    Hice la clase 1a, pero me faltan los ejercicios.

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,24 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Hello = (props) => {
+  return(
+    <div>
+      <p>Buenos días señor {props.name}, su edad es {props.age} años.</p>
     </div>
+  );
+}
+
+function App() {
+  console.log('Componente: esto funciona correctamente.')
+  const age = 28
+  const name = 'Dayo'
+  return (
+    <div>
+      <h1>Bienvenidos</h1>
+        <Hello name="Muzska" age="31" />
+        <Hello name="Alex" age={age} />
+        <Hello name="Felipe" age={12 + 12} />
+        <Hello name={name} age="27" />
+      </div>
   );
 }
 
