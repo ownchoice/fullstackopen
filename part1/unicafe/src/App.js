354346370bb31dbf24b247236aa5cb8bfec11405
ownchoice <ownchoice@users.noreply.github.com>
2020-05-06 02:09:52 -0300

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  let total = good + neutral + bad
   return (
     <>
     <h1>
@@ -30,9 +30,15 @@ const App = () => {
     <h2>
       Statistics
     </h2>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>All: {total}</li>
+      {console.log(bad * -1)}
+      <li>Average: {(good - bad) / (total)}</li>
+      <li>Positive: {(good * 100) / total}%</li>
+    </ul>
     </>
   )
 }
