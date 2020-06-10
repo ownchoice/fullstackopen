be5f4669b605f41bee8e5f0fcdfcbcf5a257e7bf
ownchoice <ownchoice@users.noreply.github.com>
2020-06-09 23:05:18 -0300

    1.14*: anecdotes step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -20,17 +20,37 @@ const App = (props) => {
     const newVotes = [...votes];
     newVotes[anecdoteIndex] += 1;
     setVotes(newVotes)
+    getMostVoted(newVotes)
   }
+    
+  const [mostVoted, setMostVoted] = useState(0);
+
+  const getMostVoted = (votesArray) => {
+    const mostVotes = Math.max(...votesArray)
+    for (var i = 0; i < votesArray.length; i++) {
+      if (votesArray[i] === mostVotes) {
+        setMostVoted(i);
+      }
+    }
+  }
+
   return (
     <div>
       <p>
-        {props.anecdotes[selected]}
+        <em>{props.anecdotes[selected]}</em>
       </p>
       <p>
         This anecdote has {votes[selected]} votes.
       </p>
       <Button handleClick={() => setSelected(getRndInteger(0, props.anecdotes.length))} text="Random" />
       <Button handleClick={() => handleVote(selected)} text="Vote" />
+      <h2>Anecdote with most votes</h2>
+      <p>
+        <em>{props.anecdotes[mostVoted]}</em>
+      </p>
+      <p>
+        With {votes[mostVoted]} votes.
+      </p>
     </div>
   )
 }
