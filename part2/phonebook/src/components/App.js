29e94500d3dfb8efdda4fbedaf38cc505db3c8e7
ownchoice <ownchoice@users.noreply.github.com>
2020-09-07 14:00:54 -0300

    2.9*: The Phonebook Step4

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-5323523' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
@@ -27,20 +30,26 @@ const App = () => {
     setNewPhonenumber(event.target.value)
   }
 
+  const [ newSearchFilter, setNewSearchFilter ] = useState('')
+  const handleNewSearchFilter = (event) => {
+    setNewSearchFilter(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>Filter shown with: <input value={newSearchFilter} onChange={handleNewSearchFilter}></input></p>
+      <h2>Add a new contact</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>number: <input value={newPhonenumber} onChange={handlePhonenumberChange} /></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <p>
+          Name: <input value={newName} onChange={handleNameChange} />
+        </p>
+        <p>Number: <input value={newPhonenumber} onChange={handlePhonenumberChange} /></p>
+        <p>
+          <button type="submit">Add</button>
+        </p>
       </form>
       <h2>Numbers</h2>
-      { persons.map(person => <p key={person.name}>{ person.name } { person.number } </p>) }
+      { persons.filter(person => person.name.toLowerCase().includes(newSearchFilter)).map(person => <p key={person.name}>{ person.name } { person.number } </p>) }
     </div>
   )
 }
