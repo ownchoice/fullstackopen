f28a0a8f3c77cdbef224a6a4927ea007d41f6a2d
ownchoice <ownchoice@users.noreply.github.com>
2020-09-07 13:45:41 -0300

    2.8: The Phonebook Step3

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -2,24 +2,29 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-5323523' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    // Array comprehensions: Non-standard. Do not use!
-    // const nameList = [ for (person of persons) person.name ]
     if (persons.some( element => element.name === newName )) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({ name: newName }))
+      setPersons(persons.concat({ name: newName, number: newPhonenumber }))
     }
     setNewName('')
+    setNewPhonenumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const [ newPhonenumber, setNewPhonenumber ] = useState('')
+
+  const handlePhonenumberChange = (event) => {
+    setNewPhonenumber(event.target.value)
   }
 
   return (
@@ -29,12 +34,13 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
+        <div>number: <input value={newPhonenumber} onChange={handlePhonenumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      { persons.map(person => <p key={person.name}>{ person.name }</p>) }
+      { persons.map(person => <p key={person.name}>{ person.name } { person.number } </p>) }
     </div>
   )
 }
