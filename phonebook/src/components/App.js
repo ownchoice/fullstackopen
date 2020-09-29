64333192e8deaf2607b19de9bc2da9df52728ff7
ownchoice <ownchoice@users.noreply.github.com>
2020-09-29 14:20:31 -0300

    Testing

--- phonebook/src/components/App.js
+++ phonebook/src/components/App.js
@@ -8,9 +8,9 @@ import Notification from './Notification'
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    // { name: 'Ada Lovelace', number: '39-44-5323523' },
+    // { name: 'Dan Abramov', number: '12-43-234345' },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
 
   const successStyle = {
