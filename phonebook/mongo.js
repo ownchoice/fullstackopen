87c50800093969629d6edc6de6c8d288122fa75e
ownchoice <ownchoice@users.noreply.github.com>
2020-09-30 19:32:36 -0300

    3.12: Command-line database

---
+++ phonebook/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+`mongodb+srv://FirstUser_Test01:${password}@cluster0.f1yw4.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+  
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(person => {
+    // console.log(result);
+    console.log(`added ${person.name}'s number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('Phonebook:');
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+
+
