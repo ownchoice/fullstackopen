bd366e72f9a85abe7edb99426833a189de16ff8b
ownchoice <ownchoice@users.noreply.github.com>
2020-10-03 23:48:53 -0300

    4.6*: helper functions and unit tests, step4

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,3 +1,6 @@
+// const lodash = require('lodash')
+// const array = require('lodash/array')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -18,8 +21,27 @@ const favoriteBlog = (blogs) => {
   return blogs.length === 0? 0: blogs.reduce(getMostPopular, 0)
 }
 
+const authorsReducer = (accumulator, currentBlog) => {
+  const foundAuthor = accumulator.find(item => item.author === currentBlog.author)
+  if (foundAuthor) {
+    foundAuthor.blogs += 1
+  } else {
+    accumulator = accumulator.concat({ author: currentBlog.author, blogs: 1})
+  }
+  return accumulator
+}
+
+
+const mostBlogs = (blogs) => {
+  const authorsList = blogs.reduce(authorsReducer, [])
+  return authorsList.reduce((accumulator, currentValue) => {
+    return currentValue.blogs > accumulator.blogs? currentValue: accumulator
+  })
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
