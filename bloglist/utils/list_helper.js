d0854d70e797efc4c6f3a8ee71ed57af8c3bf9bb
ownchoice <ownchoice@users.noreply.github.com>
2020-10-03 23:15:04 -0300

    4.5*: helper functions and unit tests, step3

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -10,14 +10,22 @@ const totalLikes = (blogs) => {
   return blogs.length === 0? 0: blogs.reduce(reducer, 0)
 }
 
-const getMaxLikes = (accumulator, currentValue) => {
-  return accumulator > currentValue.likes? accumulator: currentValue.likes
+// const getMaxLikes = (accumulator, currentValue) => {
+//   return accumulator > currentValue.likes? accumulator: currentValue.likes
+// }
+
+// const favoriteBlog = (blogs) => {
+//   const mostLikes = blogs.reduce(getMaxLikes, 0)
+//   const mostPopularBlog = blogs.find(blog => blog.likes === mostLikes)
+//   return blogs.length === 0? 0: mostPopularBlog
+// }
+
+const getMostPopular = (accumulator, currentValue) => {
+  return accumulator.likes > currentValue.likes? accumulator: currentValue
 }
 
 const favoriteBlog = (blogs) => {
-  const mostLikes = blogs.reduce(getMaxLikes, 0)
-  const mostPopularBlog = blogs.find(blog => blog.likes === mostLikes)
-  return blogs.length === 0? 0: mostPopularBlog
+  return blogs.length === 0? 0: blogs.reduce(getMostPopular, 0)
 }
 
 module.exports = {
