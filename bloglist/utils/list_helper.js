a7dd920cbc10366aaab3eb8458a896bbe7df369f
ownchoice <ownchoice@users.noreply.github.com>
2020-10-03 23:03:13 -0300

    4.4: helper functions and unit tests, step2

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const reducer = (accumulator, currentValue) => {
+  return accumulator + currentValue.likes
+}
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0? 0: blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
