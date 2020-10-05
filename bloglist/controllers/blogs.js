0cdc7f977a95e12875d2b310d3fff191a188064a
ownchoice <ownchoice@users.noreply.github.com>
2020-10-04 22:13:38 -0300

    4.10: Blog list tests, step3

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -2,22 +2,15 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+  const blogs = await Blog.find({})
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 module.exports = blogsRouter
