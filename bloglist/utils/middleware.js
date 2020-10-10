cfc0003957002367662fd76ba936442f354e512b
ownchoice <ownchoice@users.noreply.github.com>
2020-10-10 00:21:27 -0300

    4.20*: bloglist expansion, step8

--- bloglist/utils/middleware.js
+++ bloglist/utils/middleware.js
@@ -27,8 +27,21 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+    // console.log(response.token)
+  } else {
+    request.token = null
+    // console.log(response)
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
