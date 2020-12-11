441751bc1ed19009d15f8861d613f4726d091252
ownchoice <ownchoice@users.noreply.github.com>
2020-12-11 14:22:42 -0300

    5.7* Blog list frontend, step7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,26 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    <b>{blog.title}</b> by <i>{blog.author}</i> at <u>{blog.url}</u>
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisibility, setDetailsVisibility] = useState(false)
+  const changeDetailsVisibility = () => {
+    setDetailsVisibility(!detailsVisibility)
+  }
+
+  return (
+    <>
+      {detailsVisibility ? (
+        <p>
+          <b>{blog.title}</b> by <i>{blog.author}</i> at <u>{blog.url}</u> ({blog.likes} 👍){' '}<button>like</button>{' '}
+          <button onClick={changeDetailsVisibility}>hide details</button>
+        </p>
+      ) : (
+        <div>
+          {blog.title}{' '}
+          <button onClick={changeDetailsVisibility}>show details</button>
+        </div>
+      )}
+    </>
+  )
+}
 
 export default Blog
