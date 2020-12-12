d607c6b0b10e1aaa074bc69a53b0cc4191b5c0cb
ownchoice <ownchoice@users.noreply.github.com>
2020-12-12 00:01:36 -0300

    5.15*: Blog list tests, step3

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -15,10 +15,10 @@ describe('<Blog />', () => {
       author: 'Matthew',
       likes: 25,
     }
-    component = render(<Blog blog={blog} />)
   })
 
   test('renders content', () => {
+    component = render(<Blog blog={blog} />)
     const elementByClass = component.container.querySelector('.blog')
     expect(elementByClass).toBeDefined()
 
@@ -32,12 +32,28 @@ describe('<Blog />', () => {
   })
 
   test('clicking the "show details" button shows the URL and like count', async () => {
+    component = render(<Blog blog={blog} />)
     const button = component.getByText('show details')
+    expect(button).toBeDefined()
     fireEvent.click(button)
 
     expect(component.container).toHaveTextContent(blog.title)
     expect(component.container).toHaveTextContent(blog.author)
     expect(component.container).toHaveTextContent(blog.url)
     expect(component.container).toHaveTextContent(blog.likes)
+  })
+
+  test('if the like button is clicked twice, the event handler is called twice', async () => {
+    const updateBlog = jest.fn()
+    component = render(<Blog blog={blog} updateBlog={updateBlog} />)
+
+    const buttonShowDetails = component.getByText('show details')
+    fireEvent.click(buttonShowDetails)
+
+    const buttonLike = component.getByText('like')
+    expect(buttonLike).toBeDefined()
+    fireEvent.click(buttonLike)
+    fireEvent.click(buttonLike)
+    expect(updateBlog.mock.calls.length).toBe(2)
   })
 })
