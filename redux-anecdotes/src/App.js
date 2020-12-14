24715d14c14e1e158168a5c9d51d9c372541f37e
ownchoice <ownchoice@users.noreply.github.com>
2020-12-14 15:32:57 -0300

    6.3: anecdotes, step1

---
+++ redux-anecdotes/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addVoteTo, resetVotesOf } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(addVoteTo(id))
+  }
+
+  const setToZero = (id) => {
+    console.log('reset to zero votes', id)
+    dispatch(resetVotesOf(id))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => setToZero(anecdote.id)}>reset</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
