3e23edd4277e4623cc13b34ec74c4dd54c248054
ownchoice <ownchoice@users.noreply.github.com>
2020-12-15 23:09:46 -0300

    7.2: routed anecdotes, step2

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link, useRouteMatch } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -20,12 +20,38 @@ const Menu = () => {
   )
 }
 
+const Anecdote = ({ anecdote }) => {
+  if (anecdote === null) {
+    return (
+      <div>
+        <h2>Wrong anecdote ID</h2>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>
+        {anecdote.content} by {anecdote.author}
+      </h2>
+      <p>has {anecdote.votes} botes</p>
+      <p>
+        for more info see{' '}
+        <a href={anecdote.info} target='_blank' rel='noopener noreferrer'>
+          {anecdote.info}
+        </a>
+      </p>
+    </div>
+  )
+}
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+        <li key={anecdote.id}>
+          <Link to={`anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
       ))}
     </ul>
   </div>
@@ -154,11 +180,21 @@ const App = () => {
     setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)))
   }
 
+  const match = useRouteMatch('/anecdotes/:id')
+  const anecdote = match
+    ? anecdotes.find(
+        (anecdote) => Number(anecdote.id) === Number(match.params.id)
+      )
+    : null
+
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
       <Switch>
+        <Route path='/anecdotes/:id'>
+          <Anecdote anecdote={anecdote} />
+        </Route>
         <Route path='/create'>
           <CreateNew addNew={addNew} />
         </Route>
