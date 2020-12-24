33ff92573a8415acff818726a7069ea625150901
ownchoice <ownchoice@users.noreply.github.com>
2020-12-24 00:50:18 -0300

    8.19 Books by genre, part 1

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -31,9 +31,9 @@ const Authors = (props) => {
       <table>
         <tbody>
           <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
+            <th>Name</th>
+            <th>Born</th>
+            <th>Books</th>
           </tr>
           {authors.map((a) => (
             <tr key={a.name}>
--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -4,6 +4,10 @@ import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
   const [books, setBooks] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState('')
+  let genres = new Set()
+  books.forEach((book) => book.genres.forEach((genre) => genres.add(genre)))
+
   const result = useQuery(ALL_BOOKS, {
     onError: (error) => {
       console.log(error.graphQLErrors[0].message)
@@ -31,19 +35,34 @@ const Books = (props) => {
       <table>
         <tbody>
           <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books.map((book) =>
+            selectedGenre === '' ||
+            (book.genres && book.genres.includes(selectedGenre)) ? (
+              <tr key={book.title}>
+                <td>{book.title}</td>
+                <td>{book.author.name}</td>
+                <td>{book.published}</td>
+              </tr>
+            ) : null
+          )}
         </tbody>
       </table>
+      <div>
+        <h3>Filter by genre</h3>
+        {Array.from(genres).map((genre) => (
+          <button
+            key={`${genre}${Math.random().toString()}`}
+            onClick={() => setSelectedGenre(genre)}
+          >
+            {genre}
+          </button>
+        ))}
+        <button onClick={() => setSelectedGenre('')}>all</button>
+      </div>
     </div>
   )
 }
