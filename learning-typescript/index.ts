e92b3e9a0eba1317cfa6830c561e6dcab9c4b2e1
ownchoice <ownchoice@users.noreply.github.com>
2021-01-22 11:05:45 -0300

    9.6 Eslint

--- learning-typescript/index.ts
+++ learning-typescript/index.ts
@@ -1,20 +1,24 @@
-import express from 'express'
-import calculateBmi from './bmiCalculator'
-const app = express()
+import express from 'express';
+import calculateBmi from './bmiCalculator';
+// import calculator from './calculator'
+const app = express();
+// import morgan from 'morgan';
 
 if (process.env.NODE_ENV === 'development') {
-  const morgan = require('morgan')
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+  const morgan = require('morgan');
   app.use(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     morgan(
       ':method :url - status: :status - content-length: :res[content-length] - :response-time ms'
     )
-  )
+  );
   // app.use(morgan('tiny'))
-  // morgan.token('mitokenpropio', function (req: any, _res: any) {
+  // morgan.token('mitokenpropio', function (req, _res) {
   //   return JSON.stringify(req.body)
   // })
   // app.use(
-  //   morgan(function (tokens: any, req: any, res: any) {
+  //   morgan(function (tokens, req, res) {
   //     if (tokens.method(req, res) === 'POST') {
   //       return [
   //         tokens.method(req, res),
@@ -45,34 +49,41 @@ if (process.env.NODE_ENV === 'development') {
   // )
 }
 
-app.get('/', (_req: any, res: any) => {
-  res.send('Go to the <a href="/hello">hello</a> endpoint')
-})
+app.get('/', (_req, res) => {
+  res.send('Go to the <a href="/hello">hello</a> endpoint');
+});
 
-app.get('/hello', (_req: any, res: any) => {
-  res.send('Hello Full Stack!')
-})
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
 
-app.get('/bmi', (req: any, res: any) => {
-  const height = req.query.height
-  const weight = req.query.weight
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
   // console.log(height, weight)
 
-  if (height === undefined || weight === undefined) {
+  if (req.query.height === undefined || req.query.weight === undefined) {
     res.status(422).json({
       error: 'two parameters needed: weigth and height',
-    })
+    });
   } else if (Number.isNaN(Number(height)) || Number.isNaN(Number(weight))) {
     res.status(422).json({
       error: 'malformatted parameters',
-    })
+    });
   } else {
-    res.send(calculateBmi(height, weight))
+    res.send(calculateBmi(height, weight));
   }
-})
+});
 
-const PORT = 3003
+// app.get('/calculate', (req, res) => {
+//   const { value1, value2, op } = req.query
+
+//   const result = calculator(value1, value2, op)
+//   res.send(result)
+// })
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
