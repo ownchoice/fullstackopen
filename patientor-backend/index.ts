f3c3ff9019b3163c5fda29b37272b842bf33f37b
ownchoice <ownchoice@users.noreply.github.com>
2021-01-23 23:58:34 -0300

    9.9: Patientor backend, step2

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
+});
+
+app.get('/api/patients', (_req, res) => {
+  console.log('someone pinged here');
+  res.json([{
+    id: 'id 1',
+    name: 'name 1',
+    occupation: 'occupation 1',
+    gender: 'male',
+  }]);
 });
 
 app.listen(PORT, () => {
